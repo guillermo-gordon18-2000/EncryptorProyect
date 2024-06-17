e8c92a29de4a9f39a542ef7043a8e3e48c504613
guillermo gordon <ggordon@senacyt.gob.pa>
2024-06-17 01:48:15 -0500

    17/6/2024 incluide/manejo de errores

--- include/file_utils.hpp
+++ include/file_utils.hpp
@@ -1,10 +1,13 @@
 #ifndef FILE_UTILS_HPP
 #define FILE_UTILS_HPP
 
-#include <vector>
 #include <string>
+#include <vector>
 
+// Declaración de la función para leer archivos
 std::vector<unsigned char> readFile(const std::string &filename);
+
+// Declaración de la función para escribir archivos
 void writeFile(const std::string &filename, const std::vector<unsigned char> &data);
 
 #endif // FILE_UTILS_HPP
---
+++ include/openssl_utils.hpp
@@ -0,0 +1,7 @@
+// openssl_utils.hpp
+#ifndef OPENSSL_UTILS_HPP
+#define OPENSSL_UTILS_HPP
+
+void handleErrors();
+
+#endif // OPENSSL_UTILS_HPP
