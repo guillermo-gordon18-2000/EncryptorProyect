34bac67c3f59817ceb3ffb8564f132070f2ee3ad
guillermo gordon <ggordon@senacyt.gob.pa>
2024-06-17 01:48:59 -0500

    17/6/2024 src/llave para encyptar

--- src/key_derivation.cpp
+++ src/key_derivation.cpp
@@ -1,15 +1,10 @@
+// key_derivation.cpp
 #include "key_derivation.hpp"
+#include "openssl_utils.hpp"
 #include <openssl/evp.h>
-#include <openssl/sha.h>
-#include <openssl/err.h>
-
-void handleErrors() {
-    ERR_print_errors_fp(stderr);
-    abort();
-}
 
 void deriveKeyAndIV(const std::string &password, unsigned char *key, unsigned char *iv) {
-    const unsigned char salt[] = "12345678"; // Usa un salt seguro en producción
+    const unsigned char salt[] = "12345678";
     if (!PKCS5_PBKDF2_HMAC(password.c_str(), password.size(), salt, sizeof(salt), 10000, EVP_sha256(), 32, key)) {
         handleErrors();
     }
