67e34d5e42abff2be4a71af395105b1984bd09e9
guillermo-gordon18-2000 <guillermo18zgordon@gmail.com>
2024-07-01 23:41:49 -0500

    1/7/2024 Agregado Build  Lazy(Plugin)

---
+++ build/CMakeFiles/encryptor.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/encryptor.dir/src/encryption.cpp.o"
+  "CMakeFiles/encryptor.dir/src/encryption.cpp.o.d"
+  "CMakeFiles/encryptor.dir/src/file_utils.cpp.o"
+  "CMakeFiles/encryptor.dir/src/file_utils.cpp.o.d"
+  "CMakeFiles/encryptor.dir/src/key_derivation.cpp.o"
+  "CMakeFiles/encryptor.dir/src/key_derivation.cpp.o.d"
+  "CMakeFiles/encryptor.dir/src/main.cpp.o"
+  "CMakeFiles/encryptor.dir/src/main.cpp.o.d"
+  "CMakeFiles/encryptor.dir/src/mysql_utils.cpp.o"
+  "CMakeFiles/encryptor.dir/src/mysql_utils.cpp.o.d"
+  "CMakeFiles/encryptor.dir/src/openssl_utils.cpp.o"
+  "CMakeFiles/encryptor.dir/src/openssl_utils.cpp.o.d"
+  "encryptor"
+  "encryptor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/encryptor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
